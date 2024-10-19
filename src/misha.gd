c8e1cd2cf573a07f8bdbc4ce107660a47cea7d9b
ilyabasisty <ilcode@yandex.ru>
2024-10-20 02:39:40 +0300

    add stuuuuul

---
+++ src/misha.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+func play_idle_animation():
+	$AnimationPlayer.play("idle")
+
+
+func play_walk_animation():
+	$AnimationPlayer.play("walk")
+
+func reset_animation():
+	$AnimationPlayer.play("RESET")
--- src/player.gd
+++ src/player.gd
@@ -1,9 +1,19 @@
 extends CharacterBody2D
 
-
+const PLAYER_SPEED = 300
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-	velocity = direction * 500
+	velocity = direction * PLAYER_SPEED
 	move_and_slide()
+	
+	if direction[0] < 0:
+		%Misha/MishaTheAgileMan.flip_h = true
+	elif direction[0] > 0:
+		%Misha/MishaTheAgileMan.flip_h = false
+	
+	if velocity.length() > 0.0:
+		%Misha.play_walk_animation()
+	else:
+		%Misha.play_idle_animation()
