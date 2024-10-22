6123c6d76df88b23b6262e890fa7dfba40b5e24e
ilyabasisty <ilcode@yandex.ru>
2024-10-23 00:56:28 +0300

    mob movement to player

--- src/mob.gd
+++ src/mob.gd
@@ -1,4 +1,17 @@
 extends CharacterBody2D
 
+const MOB_SPEED = 200
+
+@onready var player = get_node("/root/Game/Player")
+
 func _physics_process(delta: float) -> void:
 	%Mob.play_walk_animation()
+	
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * MOB_SPEED
+	move_and_slide()
+	
+	if direction[0] < 0:
+		$Mob/TheCat.flip_h = false
+	elif direction[0] > 0:
+		$Mob/TheCat.flip_h = true
--- src/player.gd
+++ src/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const PLAYER_SPEED = 300
+const PLAYER_SPEED = 400
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
