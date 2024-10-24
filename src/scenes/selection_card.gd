dc296bb3c7d6cb7b7e89e2294c9b1ac76cc652ca
ilyabasisty <ilcode@yandex.ru>
2024-10-24 23:20:25 +0300

    guns n roses ))

--- src/scenes/selection_card.gd
+++ src/scenes/selection_card.gd
@@ -6,9 +6,13 @@ func _ready() -> void:
 func change_select_character():
 	var select = Global.CHARACTERS[Global.select_character_id]
 	var select_texture = load(select.path)
-	$TextureRect.texture = select_texture
+	var weapon_texture = load(select.weapon)
+	$mishail_texture.texture = select_texture
+	$weapon_texture.texture = weapon_texture
 	$ColorRect/title_label.text = select.title
 	$ColorRect/subtitle_label.text = select.subtitle
+	$ColorRectWeapon/title_label.text = select.weapon_title
+	$ColorRectWeapon/subtitle_label.text = select.weapon_subtitle
 	
 
 func _on_slide_back_pressed() -> void:
--- src/scripts/global.gd
+++ src/scripts/global.gd
@@ -4,12 +4,19 @@ const CHARACTERS = [
 	{
 		"title": "Михаил обыкновенный",
 		"subtitle": "Завсегдатай своих стримов, гигачад и просто хороший парень, познавший глубины танкового мастерства",
-		"path": "res://assets/images/mishas/Misha_the_agile_man.png"
+		"path": "res://assets/images/mishas/Misha_the_agile_man.png",
+		"weapon": "res://assets/images/weapons/frruuuutmotiv.png",
+		"weapon_title": "Фрутмотиловная смесь",
+		"weapon_subtitle": "Мирская реплика амброзии",
 	},
 	{
 		"title": "Крипхаил",
-		"subtitle": "Тёмная личность Михаила, запечатленная единожды на стриме МАСИГИС МОСТАРС 747 (5:19). Особо опасен",
-		"path": "res://assets/images/mishas/Misha_the_creeper.png"
+		"subtitle": "Тёмная личность Михаила, замеченная единожды на стриме МАСИГИС МОСТАРС 747 (5:19). Особо опасен",
+		"path": "res://assets/images/mishas/Misha_the_creeper.png",
+		"weapon": "res://assets/images/weapons/deep_dark_fantasy.png",
+		"weapon_title": "D.D.F. | deep dark fantasy",
+		"weapon_subtitle": "Если это ROR, то бонус к скорости, если это жизнь, то...",
+
 	}
 ]
 
